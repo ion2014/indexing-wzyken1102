00671337fc41199ed0491db7d567a92c066f87fb
Franco <fsolleza@icloud.com>
2019-11-08 14:07:02 +0000

    need to recreate indexes after update

--- TableTests.java
+++ TableTests.java
@@ -83,6 +83,11 @@ public class TableTests {
         TupleIDSet intermediate = table.filter(filter);
         table.update("B", intermediate, 2000);
 
+        // need to recreate the indexes because we don't require students to
+        // update the index
+        table.setClusteredIndex("A");
+        table.setSecondaryIndex("B");
+
         MaterializedResults results = table.materialize(attributes, null);
         TupleCollection tuples = new TupleCollection();
         for (Tuple t : results) {
@@ -97,6 +102,11 @@ public class TableTests {
         TupleIDSet intermediate = table.filter(filter);
 
         table.delete(intermediate);
+
+        // need to recreate the indexes because we don't require students to
+        // update the index
+        table.setClusteredIndex("A");
+        table.setSecondaryIndex("B");
 
         HashSet<String> attributes = new HashSet<String>();
         attributes.add("A");
