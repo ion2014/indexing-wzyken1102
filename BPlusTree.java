198137c9e88042e530ac5c6b29281b79f75f440a
wzyken1102 <zhiyu_wang@brown.edu>
2019-11-09 19:41:12 -0500

    update btree

--- BPlusTree.java
+++ BPlusTree.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.lang.*;
 
 // The BPlusTree class. You'll need to fill the methods in. DO NOT change the
 // function signatures of these methods. Our checker relies on these methods and
@@ -15,20 +16,33 @@ public class BPlusTree {
 
     // Instantiate a BPlusTree with a specific order
     public BPlusTree(Integer order) {
-        root = new Node(order, NodeType.INTERNAL);
+        root = new INode(order);
         this.order = order;
     }
 
     // Given a key, returns the value associated with that key or null if doesnt
     // exist
-    public Integer get(Integer key) { return null; }
+    public Integer get(Integer key) {
+        return root.get(key);
+    }
 
     // Insert a key-value pair into the tree. This tree does not need to support
     // duplicate keys
-    public void insert(Integer key, Integer value) { }
+    public void insert(Integer key, Integer value) {
+        if (root.nodeType() == NodeType.LEAF) {
+            Split rootSplit = root.insert(key, value);
+            if (rootSplit != null) {
+                root = new INode(order, rootSplit);
+            }
+        } else {
+            root.insert(key, value);
+        }
+    }
 
     // Delete a key and its value from the tree
-    public void delete(Integer key) { }
+    public void delete(Integer key) {
+        root.delete(key);
+    }
 
     // Optional methods to write
     // This might be a helpful function for your debugging needs
@@ -99,32 +113,34 @@ abstract class Node {
 
     // You might want to implement a search method to search for the
     // corresponding position of a given key in the node
-     abstract Integer search(Integer key) {};
+    abstract Integer search(Integer key) {};
 
+
+    abstract  Integer mid() {};
     // You might want to implement a split method for nodes that need to split.
     // We use the split class defined above to encapsulate the information
     // resulting from the split.
-     abstract Split split();          // Note the use of split here
+    abstract Split split();          // Note the use of split here
 
     // You might want to implement an insert method. We use the Split class to
     // indicate whether a node split as a result of an insert because splits in
     // lower levels of the tree may propagate upward.
-     abstract Split insert(Integer key, Integer value); // And split here
+    abstract Split insert(Integer key, Integer value); // And split here
 
     // You might want to implement a delete method that traverses down the tree
     // calling a child's delete method until you hit the leaf.
-     abstract void delete(Integer key);
+    abstract void delete(Integer key);
 
     // You might want to implement a get method that behaves similar to the
     // delete method. Here, the get method recursively calls the child's get
     // method and returns the integer up the recursion.
-     abstract Integer get(Integer key);
+    abstract Integer get(Integer key);
 
     // You might want to implement a helper function that cleans up a node. Note
     // that the keys, values, and children of a node should be null if it is
     // invalid. Java's memory manager won't garbage collect if there are
     // references hanging about.
-     abstract void cleanEntries();
+    //abstract void cleanEntries();
 }
 
 // A leaf node (LNode) is an instance of a Node
@@ -135,6 +151,10 @@ class LNode extends Node {
     // change it, your tree will not be correct according to our checker. Values
     // in this array that are not valid should be null.
     public Integer[] values;
+    public Integer[] keys;
+    private Integer order;
+    private int numChildren;
+    private Lnode rightSibling;
 
     // DO NOT edit this method;
     public NodeType nodeType() { return NodeType.LEAF; };
@@ -152,7 +172,95 @@ class LNode extends Node {
         super(order, NodeType.LEAF);
 
         // A leaf needs to instantiate the values array.
+        keys = new String[order];
         values = new Integer[order];
+        numChildren = 0;
+        rightSibling = null;
+    }
+
+    @Override
+    public Integer get(Integer key) {
+        Integer index = search(key);
+        if (index < numChildren) {
+            return values[index];
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Integer search(Integer key) {
+        Integer index = binarySearch(keys, key, numChildren);
+        if (index == numChildren - 1 && keys[index] < key) {
+            return numChildren;
+        }
+        return index;
+    }
+
+    @Override
+    public Integer mid() {
+        return order/2;
+    }
+
+    @Override
+    public Split split() {
+        LNode right = new LNode(order);
+        int index = 0;
+        for (int i = mid(); i < order; i++) {
+            right.keys[index] = this.keys[i];
+            right.values[index] = this.values[i];
+            ++index;
+        }
+        right.numChildren = index;
+        this.rightSibling = right;
+        this.numChildren -= index;
+        return new Split(keys[mid()], this, right);
+    }
+
+    @Override
+    public void delete(Integer key) {
+        Integer index = search(key);
+
+        if (keys[index] != key || index == numChildren) {
+            return;
+        }
+        for (Integer i = index; i < numChildren - 1; i++) {
+            keys[i] = keys[i+1];
+            values[i] = values[i+1];
+        }
+        --numChildren;
+    }
+
+    @Override
+    public Split insert(Integer key, Integer value) {
+        Integer index = search(key);
+        if (index == numChildren) {
+            keys[index] = key;
+            values[index] = value;
+            ++numChildren;
+        } else if (keys[index].equals(key)) {
+            values[index] = value;
+        } else {
+            Integer preKey = keys[index];
+            Integer preVal = values[index];
+            for (int i = index + 1; i < numChildren; i++) {
+                Integer tempKey = keys[i];
+                Integer tempVal = values[i];
+                keys[i] = preKey;
+                values[i] = preVal;
+                preKey = tempKey;
+                preKey = tempVal;
+            }
+            keys[index] = key;
+            values[index] = value;
+            ++numChildren;
+        }
+
+        if (numChildren == order) {
+            return this.split();
+        } else {
+            return null;
+        }
     }
 }
 
@@ -170,7 +278,10 @@ class INode extends Node {
     // node. For example, children[x].get() would work regardless of whether it
     // is a leaf or internal node if the get function is an abstract method in
     // the Node class.
-    public Node[] children; 
+    public Node[] children;
+    public Integer[] keys;
+    public Integer order;
+    public int numChildren;
 
     // DO NOT edit this method;
     public NodeType nodeType() { return NodeType.INTERNAL; };
@@ -179,14 +290,120 @@ class INode extends Node {
     // *************************************************************************
 
     // A leaf node is instantiated with an order
-    public INode(Integer order) {
+    public INode(Integer order, Split split) {
 
         // Because this is also a Node, we instantiate the Node (abstract)
         // superclass, identifying itself as a leaf.
         super(order, NodeType.INTERNAL);
-
         // An INode needs to instantiate the children array
+        children = new Node[order+1];
+        keys = new String[order];
+        if (split != null) {
+            children[0] = split.left;
+            children[1] = split.right;
+            keys[0] = split.key;
+        }
+        numChildren = 2;
     }
+
+    @Override
+    public Integer search(Integer key) {
+        Integer index = binarySearch(keys[], key, numChildren);
+        return index;
+    }
+
+    @Override
+    public Integer mid() {
+        return order/2;
+    }
+
+    @Override
+    public Split split() {
+        Inode right = new INode(order, null);
+        Integer mid = mid();
+        int index = 0;
+        for (int i = mid; i < numChildren; i++) {
+            right.keys[index] = keys[i];
+            right.children[index] = children[i];
+            ++index;
+        }
+        numChildren = mid;
+
+        return new Split(keys[mid - 1], this, right);
+    }
+
+    @Override
+    public Split insert(Integer key, Integer value) {
+        Integer index = search(key);
+        Split split = null;
+
+        if (index == numChildren - 1 || key < keys[index]) {
+            split = children[index].insert(key, value);
+        } else if (k == key[index]) {
+            split = children[index + 1].insert(key, value);
+        }
+
+        if (split == null) {
+            return split;
+        } else {
+            return insertSplit(split);
+        }
+    }
+
+    public Split insertSplit(Split splitToInsert) {
+        Integer index = search(splitToInsert.key);
+
+        if (index == numChildren - 1) {
+            keys[index] = splitToInsert.key;
+            values[index+1] = splitToInsert.right;
+        } else {
+            for (Integer i = numChildren; i > index; i++) {
+                keys[i] = keys[i - 1];
+                children[i + 1] = children[i];
+            }
+            keys[index] = splitToInsert.key;
+            children[index + 1] = splitToInsert.right;
+        }
+
+        ++numChildren;
+        if (numChildren > order) {
+            return this.split();
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void delete(Integer key) {
+        Integer index = this.search(key);
+        if (index == numChildren - 1 || key < keys[index]) {
+            children[index] = null;
+        } else if (key == keys[index]) {
+            children[index+1] = null;
+        }
+    }
+
+    @Override
+    public Integer get(Integer key) {
+        Integer index = search(key);
+        return children[index].get(key);
+    }
+}
+
+public static Integer binarySearch(Integer[] keys, Integer key, int n) {
+    Integer left = 0;
+    Integer right = n-1;
+    if (keys[righ])
+    while (left < right) {
+        Integer mid = left + (right - left)/2;
+        if (keys[mid] >= key) {
+            right = mid;
+        } else {
+            left = mid+1;
+        }
+    }
+
+    return right;
 }
 
 // This is potentially encapsulates the resulting information after a node
